4b90a5cffe243737a06c91d4e1fcf47d62a21ae0
endywezy <endywezy@gmail.com>
2023-12-13 23:33:48 -0800

    update

--- m_shell-mainfunc.c
+++ m_shell-mainfunc.c
@@ -10,40 +10,39 @@
  */
 int main(int ac, char **av)
 {
-        /*take in var declaration commands*/
-        info_t info[] = { INFO_INIT };
-        int fd = 2;
+	/*take in var declaration commands*/
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-        asm ("mov %1, %0\n\t"
-                "add $3, %0"
-                : "=r" (fd)
-                : "r" (fd));
-
-        if (ac == 2)
-        {
-                fd = open(av[1], O_RDONLY);
-                if (fd == -1)
-                {
-                        if (errno == EACCES)
-                                exit(126);
-                        if (errno == ENOENT)
-                        {
-                                /*Output custom funct*/
-                                _eputs(av[0]);
-                                _eputs(": 0: Can't open ");
-                                _eputs(av[1]);
-                                _eputchar('\n');
-                                _eputchar(BUF_FLUSH);
-                                exit(127);
-                        }
-                        /*Return vaule*/
-                        return (EXIT_FAILURE);
-                }
-                info->readfd = fd;
-        }
-        populate_env_list(info);
-        read_history(info);
-        hsh(info, av);
-        /*Return and Exit*/
-        return (EXIT_SUCCESS);
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				/*Output custom functio*/
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			/*Return vaule*/
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	/*Return and Exit*/
+	return (EXIT_SUCCESS);
 }
